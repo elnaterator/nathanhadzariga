e209ec781464301258f19ed14af5be50687cf21d
Nathan Hadzariga <n.hadzariga@gmail.com>
2016-01-05 14:51:48 -0800

    User name and email input validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true, format: { with: /\A[\w\s]+\z/i }, length: { maximum: 30 }
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,9 +4,22 @@ class UserTest < ActiveSupport::TestCase
 
   let(:user) { User.create! name: 'Bill Billson', email: 'test@test.com' }
 
-  it "should have name and email" do
+  it "should have name field with validation" do
     user.name.must_equal 'Bill Billson'
+    user.name = nil
+    assert_not user.valid?
+    user.name = 'Bill@bill.com'
+    assert_not user.valid?
+    user.name = 'Billlllllllllllllllllllllllllll' # length = 31
+    assert_not user.valid?
+  end
+
+  it "should have email field with validation" do
     user.email.must_equal 'test@test.com'
+    user.email = nil
+    assert_not user.valid?
+    user.email = 'invalid'
+    assert_not user.valid?
   end
 
 end
