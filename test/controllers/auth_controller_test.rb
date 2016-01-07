a61f36384cf9eb9f7f4e96cddb36a42b0aa6ef0a
Nathan Hadzariga <n.hadzariga@gmail.com>
2016-01-06 21:22:35 -0800

    WIP auth controller...

---
+++ test/controllers/auth_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class AuthControllerTest < ActionController::TestCase
+
+  describe 'login' do
+
+    it 'should log user in' do
+      assert false
+    end
+
+  end
+
+end
