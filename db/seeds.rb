515679f2d8efb6fb979277003f382b268a72251c
Nathan Hadzariga <n.hadzariga@gmail.com>
2016-04-14 01:13:52 -0700

    Add seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create(name: 'Nathan Hadzariga', email: 'n.hadzariga@gmail.com', password: 'password', password_confirmation: 'password', role: 'ADMIN')
+User.create(name: 'Bill Billson', email: 'bill@gmail.com', password: 'password', password_confirmation: 'password', role: 'USER')
+
+
+author = User.find_by(name: 'Nathan Hadzariga')
+Post.create(author_id: author.id, title: 'Example admin post', body: 'Body of the example admin post.')
+
+author = User.find_by(name: 'Bill Billson')
+Post.create(author_id: author.id, title: 'Example user post', body: 'Body of the example user post.')
+
+
+author = User.find_by(name: 'Nathan Hadzariga')
+post = Post.find_by(title: 'Example admin post')
+Comment.create(author_id: author.id, post_id: post.id, body: 'Example admin comment')
+
+author = User.find_by(name: 'Bill Billson')
+post = Post.find_by(title: 'Example admin post')
+Comment.create(author_id: author.id, post_id: post.id, body: 'Example user comment')
